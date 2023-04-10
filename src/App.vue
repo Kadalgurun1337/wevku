77e42404b555e353d62d4b495cb0c565d4ca34bd
Kadalgurun1337 <116977960+Kadalgurun1337@users.noreply.github.com>
2023-04-10 22:25:45 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,13 @@
 </template>
 
 <script>
-import Blog from './data/data.js'
-import ProjectData from './data/projects.js'
+
+
 
 export default {
   name: 'App',
   data () {
     return {
-      blog: Blog,
-      project: ProjectData
     }
   },
 
