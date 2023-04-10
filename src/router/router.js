ce286bafc7438d95dbb8e9d405458a2e93a68f2d
Kadalgurun1337 <116977960+Kadalgurun1337@users.noreply.github.com>
2023-04-10 21:12:06 +0700

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -2,11 +2,6 @@ import { createWebHistory, createRouter } from 'vue-router';
 
 import Home from '../components/Home.vue';
 import About from '../discover/AboutPage.vue';
-import Projects from '../discover/ProjectPage.vue';
-import Blog from '../discover/BlogPage.vue';
-import Contact from '../components/Contact.vue';
-import Allblogs from '../components/Allblogs.vue';
-import SingleProject from "../components/SingleProject.vue";
 
 import PageNotFound from "../components/PageNotFound.vue";
 
@@ -23,22 +18,6 @@ const routes = [{
     {
         path: "/about",
         component: About,
-    },
-    {
-        path: "/projects",
-        component: Projects,
-    },
-    {
-        path: "/blog",
-        component: Blog,
-    },
-    {
-        path: "/contact",
-        component: Contact,
-    },
-    {
-        path: "/blogs",
-        component: Allblogs,
     },
 
     {
